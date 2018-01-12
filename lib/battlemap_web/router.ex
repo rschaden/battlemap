e6bae03dd82b6c40f29abc07fa12b3d4fecd65f4
rschaden <rschaden@gmail.com>
2018-01-12 07:36:46 +0100

    Remove Battle Controller

--- lib/battlemap_web/router.ex
+++ lib/battlemap_web/router.ex
@@ -17,7 +17,6 @@ defmodule BattlemapWeb.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
-    resources "/battles", BattleController
   end
 
   # Other scopes may use custom stacks.
