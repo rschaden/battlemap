99d4958ab6124a6d04681ac99ef6ce3c5e790f03
rschaden <rschaden@gmail.com>
2018-01-13 21:36:50 +0100

    Display a simple Marker on a Google Map
    I had trouble using OSM, so use Google Maps for now

--- test/battlemap_web/controllers/page_controller_test.exs
+++ test/battlemap_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule BattlemapWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "BattleMap"
   end
 end
