90115cdac13e499585d69470560a8adaf2b34c85
rschaden <rschaden@gmail.com>
2018-01-13 21:36:50 +0100

    Display a simple Marker on a Google Map
    I had trouble using OSM, so use Google Maps for now

--- lib/battlemap_web/controllers/page_controller.ex
+++ lib/battlemap_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule BattlemapWeb.PageController do
   use BattlemapWeb, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render conn, "index.html", api_key: System.get_env("GOOGLE_API_KEY")
   end
 end
