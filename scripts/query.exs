af81764505ad85615449adbfd561a804171ea3a0
rschaden <rschaden@gmail.com>
2018-01-13 16:30:44 +0100

    Update script to actually insert battles into database

--- scripts/query.exs
+++ scripts/query.exs
@@ -1,10 +1,27 @@
 require IEx
 
-query= "https://query.wikidata.org/sparql?format=json&query=SELECT%20?item%20?itemLabel%20?location%20?startTime%20?endTime%20WHERE%20%7B%20%0A%20%20?item%20(wdt:P31/wdt:P279*)%20wd:Q178561%20.%0A%20%20?item%20%20wdt:P361%20wd:Q33143%20.%0A%20%20?item%20wdt:P625%20?location%20.%0A%20%20?item%20wdt:P580%20?startTime%20.%0A%20%20?item%20wdt:P582%20?endTime%20.%0A%20%20SERVICE%20wikibase:label%20%7B%20bd:serviceParam%20wikibase:language%20%22en%22%20%7D%20.%0A%7D%0ALIMIT%2010"
+query= "https://query.wikidata.org/sparql?format=json&query=SELECT%20?item%20?itemLabel%20?location%20?startTime%20?endTime%20WHERE%20%7B%20%0A%20%20?item%20(wdt:P31/wdt:P279*)%20wd:Q178561%20.%0A%20%20?item%20%20wdt:P361%20wd:Q33143%20.%0A%20%20?item%20wdt:P625%20?location%20.%0A%20%20?item%20wdt:P580%20?startTime%20.%0A%20%20?item%20wdt:P582%20?endTime%20.%0A%20%20SERVICE%20wikibase:label%20%7B%20bd:serviceParam%20wikibase:language%20%22en%22%20%7D%20.%0A%7D%0A"
 
 response = HTTPotion.get(query)
 parsed_response = Poison.decode!(response.body)
 
 fields = parsed_response["head"]["vars"]
 
-Enum.each(parsed_response["results"]["bindings"], fn battle -> Enum.each(fields, fn field -> IO.puts battle[field]["value"] end) end)
+Enum.each(parsed_response["results"]["bindings"], fn battle ->
+  name = battle["itemLabel"]["value"]
+
+  {:ok, date_time, _} = DateTime.from_iso8601(battle["startTime"]["value"])
+  start_date = DateTime.to_date(date_time)
+
+  {:ok, date_time, _} = DateTime.from_iso8601(battle["endTime"]["value"])
+  end_date = DateTime.to_date(date_time)
+
+  location_in_wkt =  String.upcase(battle["location"]["value"])
+  location = Geo.WKT.decode("SRID=4326;" <> location_in_wkt)
+
+  attributes = %{end_date: end_date, location: location, name: name, start_date: start_date}
+  case Battlemap.Main.create_battle(attributes) do
+    {:ok, battle} -> IO.puts("Created #{battle.name}")
+    {:error, _} -> IO.puts("Error")
+  end
+end)
