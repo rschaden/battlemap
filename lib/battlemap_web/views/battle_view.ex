cb41dab97e73afb6f1d7b7a4635a637eeb166b1a
rschaden <rschaden@gmail.com>
2018-01-16 22:55:17 +0100

    Add API-controller go get all battles as JSON

--- lib/battlemap_web/views/battle_view.ex
+++ lib/battlemap_web/views/battle_view.ex
@@ -1,3 +1,18 @@
 defmodule BattlemapWeb.BattleView do
   use BattlemapWeb, :view
+  alias BattlemapWeb.BattleView
+
+  def render("index.json", %{battles: battles}) do
+    %{data: render_many(battles, BattleView, "battle.json")}
+  end
+
+  def render("battle.json", %{battle: battle}) do
+    coordinates = battle.location.coordinates
+
+    %{id: battle.id,
+      name: battle.name,
+      start_date: battle.start_date,
+      end_date: battle.end_date,
+      location: %{lat: elem(coordinates, 1), lng: elem(coordinates, 0)}}
+  end
 end
