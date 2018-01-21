51bcf6456c2c94e405bf84069e4e189a04592b6f
rschaden <rschaden@gmail.com>
2018-01-22 00:12:36 +0100

    Make sure import script doesn't crash with BCE dates.
    It still won't import battles that occured BCE, but at least it doesn't stop.

--- scripts/query.exs
+++ scripts/query.exs
@@ -1,6 +1,6 @@
 require IEx
 
-query= "https://query.wikidata.org/sparql?format=json&query=SELECT%20?item%20?itemLabel%20?location%20?startTime%20?endTime%20WHERE%20%7B%20%0A%20%20?item%20(wdt:P31/wdt:P279*)%20wd:Q178561%20.%0A%20%20?item%20%20wdt:P361%20wd:Q33143%20.%0A%20%20?item%20wdt:P625%20?location%20.%0A%20%20?item%20wdt:P580%20?startTime%20.%0A%20%20?item%20wdt:P582%20?endTime%20.%0A%20%20SERVICE%20wikibase:label%20%7B%20bd:serviceParam%20wikibase:language%20%22en%22%20%7D%20.%0A%7D%0A"
+query= "https://query.wikidata.org/sparql?format=json&query=SELECT%20?item%20?itemLabel%20?location%20?startTime%20?endTime%20WHERE%20%7B%20%0A%20%20?item%20(wdt:P31/wdt:P279*)%20wd:Q178561%20.%0A%20%20?item%20wdt:P625%20?location%20.%0A%20%20?item%20wdt:P580%20?startTime%20.%0A%20%20?item%20wdt:P582%20?endTime%20.%0A%20%20SERVICE%20wikibase:label%20%7B%20bd:serviceParam%20wikibase:language%20%22en%22%20%7D%20.%0A%7D"
 
 response = HTTPotion.get(query)
 parsed_response = Poison.decode!(response.body)
@@ -10,11 +10,15 @@ fields = parsed_response["head"]["vars"]
 Enum.each(parsed_response["results"]["bindings"], fn battle ->
   name = battle["itemLabel"]["value"]
 
-  {:ok, date_time, _} = DateTime.from_iso8601(battle["startTime"]["value"])
-  start_date = DateTime.to_date(date_time)
+  start_date = case DateTime.from_iso8601(battle["startTime"]["value"]) do
+    {:ok, date_time, _} -> DateTime.to_date(date_time)
+    {:error, _} -> 'Unknown'
+  end
 
-  {:ok, date_time, _} = DateTime.from_iso8601(battle["endTime"]["value"])
-  end_date = DateTime.to_date(date_time)
+  end_date = case DateTime.from_iso8601(battle["endTime"]["value"]) do
+    {:ok, date_time, _} -> DateTime.to_date(date_time)
+    {:error, _} -> 'Unknown'
+  end
 
   location_in_wkt =  String.upcase(battle["location"]["value"])
   location = Geo.WKT.decode("SRID=4326;" <> location_in_wkt)
