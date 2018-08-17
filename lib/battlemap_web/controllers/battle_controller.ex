0aab265b997dbbb87cddc9159838e21e9e1a13a6
rschaden <rainer.schaden@kaeuferportal.de>
2018-08-17 15:48:17 +0000

    Add optional filter parameters to BattleController#index

--- lib/battlemap_web/controllers/battle_controller.ex
+++ lib/battlemap_web/controllers/battle_controller.ex
@@ -6,6 +6,11 @@ defmodule BattlemapWeb.BattleController do
 
   action_fallback BattlemapWeb.FallbackController
 
+  def index(conn, %{ "filter" => %{ "earliestDate" => earliest_date, "latestDate" => latest_date }}) do
+    battles = Main.search_battles(earliest_date, latest_date)
+    render(conn, "index.json", battles: battles)
+  end
+
   def index(conn, _params) do
     battles = Main.list_battles()
     render(conn, "index.json", battles: battles)
